aa29e514399802c0541897cc4d8ec25187084c05
Sal P <salvatpalvat@gmail.com>
2017-01-11 17:52:59 -0500

    Connecting to database with config file

--- src/backend/authenticate.js
+++ src/backend/authenticate.js
@@ -1,10 +1,17 @@
 module.exports = (json) => {
-    const db = require('mongodb')
+    const fs = require('fs')
+    const mongo = require('mongodb').MongoClient
     const crypto = require('crypto-toolkit')
     
-    //Connect to database
-    const url = ""
-    db.connect()
-    const authObj = JSON.parse(json)
+    const filename = "../../.dburl"
+    const url = fs.readFileSync(filename, 'utf-8')
     
+    //Connect to database
+    db.connect(url, (err, db) => {
+        if(err) {throw err}
+        const authObj = JSON.parse(json)
+        var collection = db.collection('Users')
+        console.log("Connected")
+        
+    })
 }

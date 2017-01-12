b855644c455fe2237d229fa3af38a4337394ce04
Sal P <salvatpalvat@gmail.com>
2017-01-11 19:48:46 -0500

    Changed authenticate() to return a boolean wrapped in JSON

--- test/unit/backend/authenticate.test.js
+++ test/unit/backend/authenticate.test.js
@@ -4,19 +4,22 @@ const authenticate = require("../../../src/backend/authenticate.js")
 
 
 describe("authenticate", () => {
-    it("returns true when auth data from user and database match", () => {
+    it("returns true wrapped in JSON when auth data from user and database match", () => {
         const authData = {username: "john_smith", password: "secretpassword11"}
         const authObj = JSON.stringify(authData)
-        expect(authenticate(authObj)).to.equal(true)
+        const expected = JSON.stringify({result: true})
+        expect(authenticate(authObj)).to.equal(expected)
     })
-    it("returns false when username is not found in database", () => {
+    
+    const expected = JSON.stringify({result: false})
+    it("returns false wrapped in JSON when username is not found in database", () => {
         const authData = {username: "fake_username", password: "password125"}
         const authObj = JSON.stringify(authData)
-        expect(authenticate(authObj)).to.equal(false)
+        expect(authenticate(authObj)).to.equal(expected)
     })
-    it("returns false when passwords do not match", () => {
+    it("returns false wrapped in JSON when passwords do not match", () => {
         const authData = {username: "john_smith", password: "password"}
         const authObj = JSON.stringify(authData)
-        expect(authenticate(authObj)).to.equal(false)
+        expect(authenticate(authObj)).to.equal(expected)
     })
 })

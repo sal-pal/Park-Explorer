a759901f6417b19c86937b41d7b62de54886323b
Sal P <salvatpalvat@gmail.com>
2017-01-11 19:07:26 -0500

    Connecting to database without config file

--- src/backend/authenticate.js
+++ src/backend/authenticate.js
@@ -1,16 +1,14 @@
 module.exports = (json) => {
     const mongo = require('mongodb').MongoClient
     const crypto = require('crypto-toolkit')
-    const path = require('path')
-    const fs = require('fs')
     
-    const filepath = "../../.dburl"
-    const url = fs.readFileSync(filepath, 'utf-8')
+    const url = "mongodb://user1:password1@ds145828.mlab.com:45828/salsdatabase"
     
     //Connect to database
     mongo.connect(url, (err, db) => {
         if(err) {throw err}
         const authObj = JSON.parse(json)
         var collection = db.collection('Users') 
+        console.log("Connected")
     })
 }

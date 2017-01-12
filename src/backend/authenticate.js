07d73a6aa855bed47168d65a0ae62cd61bead5fb
Sal P <salvatpalvat@gmail.com>
2017-01-11 19:03:01 -0500

    Database connection with config file not working

--- src/backend/authenticate.js
+++ src/backend/authenticate.js
@@ -1,17 +1,16 @@
 module.exports = (json) => {
-    const fs = require('fs')
     const mongo = require('mongodb').MongoClient
     const crypto = require('crypto-toolkit')
+    const path = require('path')
+    const fs = require('fs')
     
-    const filename = "../../.dburl"
-    const url = fs.readFileSync(filename, 'utf-8')
+    const filepath = "../../.dburl"
+    const url = fs.readFileSync(filepath, 'utf-8')
     
     //Connect to database
-    db.connect(url, (err, db) => {
+    mongo.connect(url, (err, db) => {
         if(err) {throw err}
         const authObj = JSON.parse(json)
-        var collection = db.collection('Users')
-        console.log("Connected")
-        
+        var collection = db.collection('Users') 
     })
 }

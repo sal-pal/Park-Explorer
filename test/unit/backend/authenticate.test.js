a48eee7d3850312ed2b83432ed390ec2c8e261c8
Sal P <salvatpalvat@gmail.com>
2017-01-27 19:38:15 -0500

    Created almost complete tests for authenticate. Only need expectations to complete the tests

--- test/unit/backend/authenticate.test.js
+++ test/unit/backend/authenticate.test.js
@@ -2,24 +2,49 @@ const chai = require('chai')
 const expect = chai.expect
 const authenticate = require("../../../src/backend/authenticate.js")
 
+//Database dependencies and data
+const mongo = require('mongodb').MongoClient
+const url = "mongodb://user1:password1@ds145828.mlab.com:45828/salsdatabase"
+const dumbyData = {username: 'actual_username', password: 'actual_password'}
+
+
+
 
 describe("authenticate", () => {
-    it("returns true wrapped in JSON when auth data from user and database match", () => {
-        const authData = {username: "john_smith", password: "secretpassword11"}
-        const authObj = JSON.stringify(authData)
-        const expected = JSON.stringify({result: true})
-        expect(authenticate(authObj)).to.equal(expected)
+    before(() => {
+        mongo.connect(url, (err, db) => {
+            if (err) {throw err}
+            db.collection('Users').insert(dumbyData)
+            db.close()
+        })        
     })
-    
-    const expected = JSON.stringify({result: false})
-    it("returns false wrapped in JSON when username is not found in database", () => {
-        const authData = {username: "fake_username", password: "password125"}
-        const authObj = JSON.stringify(authData)
-        expect(authenticate(authObj)).to.equal(expected)
+    after(() => {
+        mongo.connect(url, (err, db) => {
+            if (err) {throw err}
+            db.collection('Users').remove(dumbyData)
+            db.close()
+        })    
     })
-    it("returns false wrapped in JSON when passwords do not match", () => {
-        const authData = {username: "john_smith", password: "password"}
-        const authObj = JSON.stringify(authData)
-        expect(authenticate(authObj)).to.equal(expected)
+    it("returns 'success' when username and password are both found in database", () => {
+        //Goal: Create tests for our plan.
+        const authData = dumbyData
+        const json = JSON.stringify(authData)
+        const expected = JSON.stringify({result: 'success'})
+        expect(authenticate(json))
+    })
+    it("returns 'failure' when username is not found in database", () => {
+        const authData = {username: "fake_username", password: "actual_password"}
+        const json = JSON.stringify(authData)
+        expect(authenticate(json))
+    })
+    it("returns 'failure' when password is not found in database", () => {
+        const authData = {username: "actual_username", password: "fake_password"}
+        const json = JSON.stringify(authData)
+        expect(authenticate(json))
+    })
+    it("returns 'failure' when username and password are both not found in database", () => {
+        const authData = {username: "fake_username", password: "fake_password"}
+        const json = JSON.stringify(authData)
+        expect(authenticate(json))
     })
 })

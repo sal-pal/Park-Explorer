e17950f13048cb2ce8a04573c2bf7f87350a691f
Sal P <salvatpalvat@gmail.com>
2017-01-30 17:46:38 -0500

    Forgot to commit this earlier. authenticate.js is awaiting for failing tests

--- src/backend/authenticate.js
+++ src/backend/authenticate.js
@@ -1,3 +1,12 @@
-module.exports = (json) => {
+function authenticate (json, callback) {
+    callback(1)
+}
 
+module.exports = (json) => {
+    return new Promise((resolve, reject) => {
+        authenticate(json, (err, result) => {
+            if (err) {reject(err)}
+            resolve(result)
+        })
+    })
 }

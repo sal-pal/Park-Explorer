7a38fdc13a828839bc02c6ff696d22274bef5dd8
Sal P <salvatpalvat@gmail.com>
2017-01-30 19:32:41 -0500

    queryDatabase.test.js hooks now working

--- test/unit/backend/queryDatabase.test.js
+++ test/unit/backend/queryDatabase.test.js
@@ -5,27 +5,36 @@ const queryDatabase = require('../../../src/backend/queryDatabase.js')
 //Database dependencies and data
 const mongo = require('mongodb').MongoClient
 const url = "mongodb://user1:password1@ds145828.mlab.com:45828/salsdatabase"
-const singleResult = {queryGroup: "single_result"}
+
 
 
 
 
 describe("queryDatabase", () => {    
-    before(() => {
+    const docs = [{queryGroup: "single_result"}, {queryGroup: "multi_result1"}, {queryGroup: "multi_result2"}, {queryGroup: "multi_result3"}]    
+    before((done) => {
         mongo.connect(url, (err, db) => {
-            if (err) {throw err}
-            db.collection('Users').insert(singleResult)
+            if (err) {done(err)}
+            db.collection('Users').insert(docs)
             db.close()
+            done()
         })       
     })
-    after(() => {
+    after((done) => {
         mongo.connect(url, (err, db) => {
-            if (err) {throw err}
-            db.collection('Users').remove(singleResult)
+            if (err) {done(err)}
+            for (var i=0; i < docs.length; i++) {
+                db.collection('Users').remove(docs[i])  
+            }
             db.close()
-        })               
+            done()
+        })    
     })
     it("returns false when username not found in database", (done) => {
         const query = {username: "fake_username"}
     })
 })
+
+
+
+//Goal: How to deconstruct an array into a list of arguments

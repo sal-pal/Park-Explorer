fcdfd5e6f7eced7fb7fcf5e0dbf51d6c67d2e2e9
Sal P <salvatpalvat@gmail.com>
2017-02-01 18:30:38 -0500

    Created successful version of queryDatabase() with passing tests

--- src/backend/queryDatabase.js
+++ src/backend/queryDatabase.js
@@ -16,32 +16,3 @@ module.exports = (query, collectionName, uri) => {
             })
     })
 }
-
-
-
-/**
-        +Connecting to the database from scratch, so need all things in order to connect to database
-        +Input: URI, collection name, and query object
-        +Signature: queryDatabase(query, collectionName, uri)
-            -Do I need to supply arguments to all parameters?
-            -The function will need these arguments in order to perform the behavior. Therefore, go into test and provide the 
-            arguments to all the functions
-            
-            1) Connect to database
-            2) Get data from database
-            3) Check if have single object
-                if true:
-                    return single object
-                if have two objects or more:
-                    return the array containing javascript objects
-                if have no objects in array:
-                    return null
-                    
-            
-            +convertCursorToDoc only returns only one object. checkUsername expects only one object as well
-            +Should even contain checkPassword and checkUsername? These functions are not relevant right now.
-            +Should focus only on queryDatabase.
-            +
-                    
-            
-**/

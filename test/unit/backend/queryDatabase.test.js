57d771186212709b13e1136b7c944a885e92d064
Sal P <salvatpalvat@gmail.com>
2017-02-01 12:19:50 -0500

    Createda successful first test case. Now creating the second test case.

--- test/unit/backend/queryDatabase.test.js
+++ test/unit/backend/queryDatabase.test.js
@@ -1,29 +1,64 @@
 const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
 const expect = chai.expect
 const queryDatabase = require('../../../src/backend/queryDatabase.js')
+
+//Database dependencies and data
 const mongo = require('mongodb').MongoClient
 const url = "mongodb://user1:password1@ds145828.mlab.com:45828/salsdatabase"
 
-const singleResult = {queryGroup: 'single_result'}
-const mulitDoc = null
+//Configuring chai to use chai-as-promised
+chai.use(chaiAsPromised)
 
 
 
-describe("queryDatabase", () => {
-    before(() => {
+
+
+describe("queryDatabase", () => {    
+    const docs = [{queryGroup: "single_result"}, {queryGroup: "multi_result"}, {queryGroup: "multi_result"}, {queryGroup: "multi_result"}]
+    before((done) => {
         mongo.connect(url, (err, db) => {
-            if (err) {throw err}
-            db.collection('Users').insert(singleResult)
+            if (err) {done(err)}
+            db.collection('Users').insert(docs)
             db.close()
-        })        
+            done()
+        })       
+    })
+    after((done) => {
+        mongo.connect(url, (err, db) => {
+            if (err) {done(err)}
+            for (var i=0; i < docs.length; i++) {
+                db.collection('Users').remove(docs[i])  
+            }
+            db.close()
+            done()
+        })    
     })
     it("returns a single object for a query result of a single document", () => {
-        
+        const promise = queryDatabase({queryGroup: "single_result"})
+        return expect(promise).to.eventually.have.property("queryGroup").to.equal("single_result")
     })
     it("returns an array of objects that corresponds to a query result of muliple documents", () => {
-        
+        const promise = queryDatabase({queryGroup: "multi_result"})
+        const result = docs.slice(1,4)
+        return expect(promise).to.eventually.deep.equal([{queryGroup: "multi_result"}, {queryGroup: "multi_result"}, {queryGroup: "multi_result"}])
     })
     it('returns null when no documents are found', () => {
         
-    })    
+    }) 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

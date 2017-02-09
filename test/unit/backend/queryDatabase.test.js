bb21aa05e08cc15f0dfae7ec29aa471141b5e81a
Sal P <salvatpalvat@gmail.com>
2017-02-08 19:27:54 -0500

    Placed promise assertion outside of connect() promise's callback since promise assertions cannot be performed within callbacks

--- test/unit/backend/queryDatabase.test.js
+++ test/unit/backend/queryDatabase.test.js
@@ -38,37 +38,26 @@ describe("queryDatabase", () => {
         })    
     })
     it("returns a single object for a query result of a single document", () => {
-        connect(url)
-            .then((db) => {
-                const query = {queryGroup: "single_result"}
-                const promise = queryDatabase(query, 'Users', db)
-                return expect(promise).to.eventually.have.property("queryGroup").to.equal("single_result")
-            })
+        const promise = connect(url).then((db) => {
+            const query = {queryGroup: "single_result"}
+            return queryDatabase(query, 'Users', db)
+        })
+        return expect(promise).to.eventually.have.property("queryGroup").to.equal("single_result")
 
     })
     it("returns an array of objects that corresponds to a query result of muliple documents", () => {
-        connect(url)
-            .then((db) => {
-                const query = {queryGroup: "multi_result"}
-                const promise = queryDatabase(query, 'Users', db)
-                const result = docs.slice(1,4)
-                return expect(promise).to.eventually.deep.equal(result)
-            })
+        const promise = connect(url).then((db) => {
+            const query = {queryGroup: "multi_result"}
+            return queryDatabase(query, 'Users', db)
+        })
+        const result = docs.slice(1,4)
+        return expect(promise).to.eventually.deep.equal(result)
     })
     it('returns null when no documents are found', () => {
-        connect(url)
-            .then((db) => {
-                const query = {queryGroup: "no_results"}
-                const promise = queryDatabase(query, 'Users', db)
-                expect(promise).to.eventually.be.null
-            })
+        const promise = connect(url).then((db) => {
+            const query = {queryGroup: "no_results"}
+            return queryDatabase(query, 'Users', db)
+        })
+        expect(promise).to.eventually.be.null
     }) 
 })
-
-
-/**
-        -Problem: Mocha not find tests because they are not placed directly into describe callback
-            +Therefore, how to pass db object without nesting the tests and without binding db to global variable
-            +Have solution, although it is not the most elegant
-                -get db by using connect()
-**/

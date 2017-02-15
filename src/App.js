67ea8f9b72f758c31e492a3c67d185a08fbfef07
Sal P <salvatpalvat@gmail.com>
2017-02-15 00:53:05 -0500

    Working on handleSubmit() of Login Component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,49 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './App.css'
 
+
+
+class Login extends Component {
+    
+    constructor(props) {
+        super(props)
+    }
+    
+    handleSubmit() {
+        
+        const endpoint = this.props.endpoint
+        const callback = this.props.onArrivalOfAuthenticationResults
+        
+        //Encrypt password (need to encrpt the passwords on the backend. Do that before encrypting the front end password)
+        
+        //Make the API Call
+        fetch(endpoint).then(callback)
+        
+        //When results recieved, invoke the callback
+        
+    }
+    
+    render() {
+        return (
+            <div className="Login">
+                <h1>{this.props.title}</h1>
+                <input type="text" className="formElem" placeholder="Username"/>
+                <input type="text" className="formElem" placeholder="Password"/>
+                <button className="formElem" onClick={() => this.handleSubmit()}>Submit</button>
+                <a href="#">Sign Up</a> 
+            </div>
+        )
+    }
+}
+
+
+
 class App extends Component {  
   render() {
     return (
       <div className="App">
-        
-        <h1> 
-           Welcome to Park Forum! <br/>
-           Please Log In
-        </h1>
-        
-        <input type="text" className="formElem" placeholder="Username"/>
-        <input type="text" className="formElem" placeholder="Password"/>
-        <button className="formElem">Sumbit</button>
-        <a href="#">Sign Up</a>
+        <img className="mountains" src="https://julieshannonfuller.com/wp-content/uploads/2014/08/jsf-mountains.png"/>
+        <Login title="Hello"/>
       </div>
     );
   }

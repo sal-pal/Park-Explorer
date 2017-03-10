c0dd5b815e23131d76343b8454691965912fe9c7
Sal P <salvatpalvat@gmail.com>
2017-03-10 18:04:29 -0500

    Finished with html and css

--- src/ParkTinder.js
+++ src/ParkTinder.js
@@ -9,13 +9,34 @@ class ParkTinder extends Component {
     }
     
     render() {
+        const imgStyle = {
+            width: "500px",
+            height: "500px"
+        }
+        const descriptionStyle = {
+            maxWidth: "700px",
+            margin: "0px auto",
+            position: 'relative',
+            top: '25px',
+            fontSize: '20px'
+        }
+        const parkLinkStyle = {
+            position: 'relative',
+            top: '35px'
+        }
+        const resrveLinkStyle = {
+            display: 'block',
+            position: 'relative',
+            top: '38px'            
+        }
+        
         return (
             <div className="ParkTinder">
                 <h1>Title</h1>
-                <img src="https://julieshannonfuller.com/wp-content/uploads/2014/08/jsf-mountains.png"/>
-                <p>Visit Yellowstone and experience the worlds first national park. Marvel at a volcano’s hidden power rising up in colorful hot springs, mudpots, and geysers. Explore mountains, forests, and lakes to watch wildlife and witness the drama of the natural world unfold. Discover the history that led to the conservation of our national treasures “for the benefit and enjoyment of the people.</p>
-                <a href="#">Park Website</a>
-                <a href="#">Book a Reservation</a>
+                <img style={imgStyle} src="https://www.nps.gov/common/uploads/structured_data/3C7CCAA8-1DD8-B71B-0BE7C1B0BC469D34.jpg"/>
+                <p style={descriptionStyle}>Visit Yellowstone and experience the worlds first national park. Marvel at a volcano’s hidden power rising up in colorful hot springs, mudpots, and geysers. Explore mountains, forests, and lakes to watch wildlife and witness the drama of the natural world unfold. Discover the history that led to the conservation of our national treasures “for the benefit and enjoyment of the people.</p>
+                <a href="#" style={parkLinkStyle}>Park Website</a>
+                <a href="#" style={resrveLinkStyle}>Book a Reservation</a>
             </div>
         )
     }

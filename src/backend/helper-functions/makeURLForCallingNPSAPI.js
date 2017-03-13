7ca21e41611f052866437951b159abc094f0def2
Sal P <salvatpalvat@gmail.com>
2017-03-13 17:14:36 -0400

    Created passing tests for type checking feature of makeURLForCallingNPSAPI()

--- src/backend/helper-functions/makeURLForCallingNPSAPI.js
+++ src/backend/helper-functions/makeURLForCallingNPSAPI.js
@@ -1,3 +1,16 @@
-module.exports = () => {
+module.exports = (parkCode, resourceProperties) => {
+    const parkCodeIsString = (typeof parkCode === 'string')
+    const resourcePropertiesIsString = (typeof resourceProperties === 'array')
+    if (parkCodeIsString && resourcePropertiesIsString) {
+        //makeURL()
+    }
+    else if (!parkCodeIsString) {
+        throw new TypeError("Need to pass a string for parkCode parameter")
+    }
+    throw new TypeError("Need to pass an array for resourceProperties parameter")
+}
+
+
+function makeURL (parkCode, resourceProperties) {
     
 }

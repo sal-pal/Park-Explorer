d5619350e5168a36b6ea2c08a229c9747cdb92c1
Sal P <salvatpalvat@gmail.com>
2017-03-27 18:57:18 -0400

    Updated docs

--- src/backend/authenticate.js
+++ src/backend/authenticate.js
@@ -2,13 +2,13 @@
         Checks to see if a user's credentials are stored in the database
         
             Arguments:
-                json:  a json string containing a user's credentials (visit authenticate.test.js
+                credentials:  an object literal containing a user's credentials (visit authenticate.test.js
                        for an example).
                 
                 db:  a database object    
             
             Return Values:
-                Returns a promise whose fulfillment values are discussed in authenticate.test.js
+                Returns a promise whose fulfillment value is discussed in authenticate.test.js
 **/
 
 

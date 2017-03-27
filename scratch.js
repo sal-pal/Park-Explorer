948ff9d10d6b14092e3a353925821442019ea8a3
Sal P <salvatpalvat@gmail.com>
2017-03-27 15:17:35 -0400

    Removed all irrelvant files

--- scratch.js
+++ scratch.js
@@ -1,26 +1,6 @@
-const mongo = require('mongodb').MongoClient
-const url = "mongodb://user1:password1@ds145828.mlab.com:45828/salsdatabase"
-
-function* parentFunct(url, generator) {
-    var generator = generator(url)
-    generator.next()
-    const output = generator.next()
-    return output.value
-}
-
-//Goal: define generator
-//  !) Define in parentFunct
-//  2) yield while connecting to db
-//  3) return output of connection callback
-
-const callback = (err, db) => {
-    if (err) {throw err}
-    var collection = db.collection('Users')
-}
-
-const result = parentFunct(url, () => {
-    const output = yield mongo.connect(url, callback)
-    return output
-})
-
-console.log(result.constructor)
+/**                            
+        Monday's Objectives: Finish Application & Create Resume for Ethica
+            +Finish Application
+                1) Get application files back to original format
+                2) Implement solution to CORS problem
+**/ 

daa6faf38a98b3a8a22763f7cea2ea284a6ed857
srpalomino <salvatpalvat@gmail.com>
2017-03-28 10:45:46 -0700

    Delete scratch.js

--- scratch.js
+++
@@ -1,6 +0,0 @@
-/**                            
-        Monday's Objectives: Finish Application & Create Resume for Ethica
-            +Finish Application
-                1) Get application files back to original format
-                2) Implement solution to CORS problem
-**/ 
